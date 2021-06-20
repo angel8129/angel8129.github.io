a01c84a34df01407336a591a5fa246ecb2636fa8
angel8129 <5588129@163.com>
2021-06-20 13:25:15 +0800

    Initial commit

--- main.js
+++ main.js
@@ -1,100 +1,117 @@
-// Initialize Phaser, and creates a 400x490px game
+// 初始化 Phaser，并创建一个 400x490px 的游戏
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 var game_state = {};
 
-// Creates a new 'main' state that will contain the game
+var localStorageName = "crackalien";
+var highScore;
+
+// 创建一个包含游戏的新'main'状态
 game_state.main = function() { };  
 game_state.main.prototype = {
 
-    // Function called first to load all the assets
+    // 首先调用'preload'函数以加载所有资产
     preload: function() { 
-        // Change the background color of the game
-        this.game.stage.backgroundColor = '#71c5cf';
+        this.game.load.image('bg','assets/bg_day.png');
+        this.bg = this.game.add.sprite(0, 0, 'bg');
 
-        // Load the bird sprite
+        var style2 = { font: "30px Noto Sans SC", fill: "#000000" };
+        this.title = this.game.add.text(80, 20, " 是男人就飞100秒", style2);
+
+        // 更改游戏背景颜色
+        //this.game.stage.backgroundColor = '#71c5cf';
+
+        // 加载鸟精灵
         this.game.load.spritesheet('bird', 'assets/bird.png', 34, 24);
 
-        // Load the pipe sprite
+        // 加载管道精灵
         this.game.load.image('pipe', 'assets/pipe.png');
 
-        this.game.load.audio('jump', 'assets/jump.mp3'); 
+        this.game.load.audio('jump', 'assets/fly.wav');
+        
+        this.game.load.audio('hit', 'assets/crash.wav');
 
-        //this.game.load.audio('bgm','assets/BGM.m4a');
-        //this.bgm = this.game.add.audio('bgm',0.8,true);
-        //this.bgm.play();
+        this.game.load.audio('coin', 'assets/score.wav');
+
+        this.game.load.audio('bgm','assets/BGM.m4a');
+        this.bgm = this.game.add.audio('bgm',0.8,true);
+        this.bgm.play();
     },
 
-    // Fuction called after 'preload' to setup the game 
+    // 'preload'后调用'create'函数来设置游戏
     create: function() { 
-        // Display the bird on the screen
+        // 显示鸟
         this.bird = this.game.add.sprite(100, 245, 'bird');
         this.bird.scale.setTo(2,2); 
-        this.bird.animations.add('wing');
-        this.bird.animations.play('wing', 24, true);
+        this.bird.animations.add('bird');
+        this.bird.animations.play('bird', 24, true);
 
-        // Add gravity to the bird to make it fall
+        // 给鸟增加重力以使其坠落
         this.bird.body.gravity.y = 1000; 
 
-         // Move the anchor to the left and downward
+         // 向左和下移动锚点
         this.bird.anchor.setTo(-0.2, 0.5); 
 
-        // Call the 'jump' function when the spacekey is hit
+        // 按下空格键时调用'jump'函数
         //定义空格的热键，按下空格，则添加跳跃给鸟
        //var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
        //space_key.onDown.add(this.jump, this);
 
 
         //上面的是键盘事件，这个是触摸事件
-        var t = this
+        var t = this;
         this.game.input.touch.onTouchStart = function(){
             //alert(0);
-            t.jump();
+           t.jump();
             //console.log("down");
         }
 
-        // Create a group of 20 pipes
+        // 创建一组 20 个管道
         this.pipes = game.add.group();
         this.pipes.createMultiple(20, 'pipe');  
 
-        // Timer that calls 'add_row_of_pipes' ever 1.5 seconds
+        // 每 1.5 秒调用一次“add_row_of_pipes”的计时器
         this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);           
 
-        // Add a score label on the top left of the screen
+        // 在屏幕左上角添加分数标签
         this.score = 0;
         var style = { font: "30px Arial", fill: "#ffffff" };
         this.label_score = this.game.add.text(20, 20, "0", style);
         
-        this.jumpSound = this.game.add.audio('jump'); 
+        this.jumpSound = this.game.add.audio('jump',0.8); 
+        
+        this.hitSound = this.game.add.audio('hit',0.5); 
+
+        this.scoreSound = this.game.add.audio('coin',0.2); 
     },
 
-    // This function is called 60 times per second
+    // 此函数每秒调用 60 次
     update: function() {
-        // If the bird is out of the world (too high or too low), call the 'restart_game' function
+        // 如果小鸟飞离区域（太高或太低），调用“restart_game”函数
         if (this.bird.inWorld == false)
             this.restart_game(); 
 
         if (this.bird.angle < 20)
             this.bird.angle += 1; 
 
-        // If the bird overlap any pipes, call 'hitPipe'
+        // 如果鸟与任何管道触碰，请调用“hitPipe”
         this.game.physics.overlap(this.bird, this.pipes, this.hitPipe, null, this);      
     },
 
-    // Make the bird jump 
+    // 让小鸟飞跃
     jump: function() {
         if (this.bird.alive == false)
             return;
 
-        // Add a vertical velocity to the bird
+        // 给鸟增加一个垂直速度
         this.bird.body.velocity.y = -350;
 
-        // Create an animation on the bird
+        // 创建鸟的动画
         var animation = game.add.tween(this.bird);
 
-        // Change the angle of the bird to -20° in 100 milliseconds
+        // 在 100 毫秒内将鸟的角度更改为 -20°
         animation.to({angle: -20}, 100);
 
-        // And start the animation
+        // 并开始动画
         animation.start(); 
 
         game.add.tween(this.bird).to({angle: -20}, 100).start(); 
@@ -103,48 +120,52 @@ game_state.main.prototype = {
     },
 
     hitPipe: function() {
-        // If the bird has already hit a pipe, do nothing
-        // It means the bird is already falling off the screen
+        // 如果鸟已经撞到管子，什么都不做
+        // 这意味着小鸟已经掉下来了
         if (this.bird.alive == false)
             return;
     
-        // Set the alive property of the bird to false
+        // 将小鸟的alive属性设置为false
         this.bird.alive = false;
     
-        // Prevent new pipes from appearing
+        // 防止出现新管道
         game.time.events.remove(this.timer);
     
-        // Go through all the pipes, and stop their movement
+        // 穿过所有的管道，并停止它们的移动
         this.pipes.forEach(function(p){
             p.body.velocity.x = 0;
         }, this);
+
+        this.hitSound.play(); 
     }, 
 
-    // Restart the game
+    // 重新开始游戏
     restart_game: function() {
-        // Remove the timer
+        this.bgm.stop();
+
+        // 移除计时器
         this.game.time.events.remove(this.timer);
 
-        // Start the 'main' state, which restarts the game
+        // 启动'main'状态，重新启动游戏
         this.game.state.start('main');
     },
 
-    // Add a pipe on the screen
+    // 向游戏区添加管道
     add_one_pipe: function(x, y) {
-        // Get the first dead pipe of our group
+        // 拿到我们组的第一个预置管道
         var pipe = this.pipes.getFirstDead();
 
-        // Set the new position of the pipe
+        // 设置管道的新位置
         pipe.reset(x, y);
 
-         // Add velocity to the pipe to make it move left
+         // 向管道添加速度以使其向左移动
         pipe.body.velocity.x = -200; 
                
-        // Kill the pipe when it's no longer visible 
+        // 当它不再可见时删除管道
         pipe.outOfBoundsKill = true;
     },
 
-    // Add a row of 6 pipes with a hole somewhere in the middle
+    // 添加一排 6 个管道，中间有一个通路
     add_row_of_pipes: function() {
         var hole = Math.floor(Math.random()*5)+1;
         
@@ -153,10 +174,12 @@ game_state.main.prototype = {
                 this.add_one_pipe(400, i*60+10);   
     
         this.score += 1;
-        this.label_score.content = this.score;  
+        this.label_score.content = this.score; 
+        
+        this.scoreSound.play();
     },
 };
 
-// Add and start the 'main' state to start the game
+// 添加并启动'main'状态以开始游戏
 game.state.add('main', game_state.main);  
 game.state.start('main'); 
